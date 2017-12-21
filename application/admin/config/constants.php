1e664550e43066aa7d7fbbea7cb059e0d9bbab34
Aabshar <aabshar.forever@gmail.com>
2017-12-22 00:31:41 +0530

    date default set

--- application/admin/config/constants.php
+++ application/admin/config/constants.php
@@ -6,6 +6,7 @@ define('AUTHBY', "Parking Pass Vendor");
 define('SPONSEREDBY', "xxxxxxxxxxxxxx");
 define('REPORT_FETCH_HOURS', 48);
 
+
 /*
 |--------------------------------------------------------------------------
 | Display Debug backtrace
@@ -89,3 +90,6 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+date_default_timezone_set('Asia/Calcutta');
